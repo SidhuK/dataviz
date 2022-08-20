b37159c587d6e5e0821e595d6f39e387e01e9d67
Karat Sidhu <karatsingh@gmail.com>
2022-08-20 19:33:22 +0530

    added a bit more

--- 2022/22_August_16/character.R
+++ 2022/22_August_16/character.R
@@ -1,9 +1,10 @@
 characters <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-08-16/characters.csv")
 psych_stats <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-08-16/psych_stats.csv")
+myers_briggs <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-08-16/myers_briggs.csv")
 
 
 library(tidyverse)
 
-lotr_psych <- psych_stats |>
+lotr_psych <- myers_briggs |>
     filter(uni_name == "Lord of the Rings")
 # filter(question == "cool/dorky")
