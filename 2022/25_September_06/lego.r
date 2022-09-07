ab9603ce0e6d52943477108dcdfa8a99227d1611
Karat Sidhu <karatsingh@gmail.com>
2022-09-07 13:38:31 +0530

    added lego data

---
+++ 2022/25_September_06/lego.r
@@ -0,0 +1,3 @@
+inventories <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-06/inventories.csv.gz")
+inventory_sets <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-06/inventory_sets.csv.gz")
+sets <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-06/sets.csv.gz")
