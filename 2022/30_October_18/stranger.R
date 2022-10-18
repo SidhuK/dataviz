d2ed10563b2095845e16b56c8364fed734337b51
Karat Sidhu <karatsingh@gmail.com>
2022-10-18 19:17:06 +0530

    finished graph

--- 2022/30_October_18/stranger.R
+++ 2022/30_October_18/stranger.R
@@ -4,20 +4,10 @@ dialogue <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/t
 
 library(tidyverse)
 library(tidytext)
-
-# dialogue_most <- dialogue |>
-#     na.omit() |>
-#     group_by(stage_direction) |>
-#     count() |>
-#     arrange(desc(n)) |>
-#     head(25)
-
-# dialogue |>
-#     na.omit() |>
-#     mutate(duration = end_time - start_time) |>
-#     group_by(stage_direction, duration) |>
-#     summarise(total_time = n(stage_direction)) |>
-#     arrange(desc(total_time))
+library(showtext)
+showtext.auto(enable = TRUE)
+showtext_opts(dpi = 250)
+font_add_google("Girassol", "Girassol")
 
 dialogue_clean <- dialogue |>
     select(dialogue, season, episode, line) |>
@@ -42,6 +32,9 @@ most_common |>
     group_by(sentiment) |>
     summarise(n())
 
+most_common <- most_common |>
+    mutate(n_new = ifelse(sentiment == "positive", n, -n))
+
 dialogue_j <- dialogue_join |> mutate(sent = ifelse(sentiment == "positive", 1, -1))
 
 # dialogue_j |>
@@ -53,7 +46,51 @@ dialogue_j <- dialogue_join |> mutate(sent = ifelse(sentiment == "positive", 1, 
 #     )
 
 
-most_common |> ggplot(aes(y = fct_reorder(word, n), x = n, fill = sentiment)) +
+most_common |> ggplot(aes(x = fct_reorder(word, -n), y = n_new, fill = sentiment)) +
     geom_col() +
+    geom_text(aes(label = word, color = sentiment, hjust = ifelse(n_new < 0, 1, 0)),
+        vjust = 0.5, , size = 6,
+        family = "Girassol", angle = 90
+    ) +
+    ggtext::geom_richtext(
+        data = data.frame(
+            x = 40, y = 800,
+            label = "Stranger Things"
+        ), mapping = aes(
+            x = x,
+            y = y, label = label
+        ), size = 14, family = "Girassol",
+        inherit.aes = FALSE,
+        color = "#f03444",
+        fill = NA,
+        label.size = 0
+    ) +
+    ggtext::geom_richtext(
+        data = data.frame(
+            x = 40, y = 690,
+            label = "Sentiment Analysis of Stranger Things Dialogue<br>
+            using TidyText. Analyzed the 75 most common words occurring in <br> the first
+            4 Seasons. "
+        ), mapping = aes(
+            x = x,
+            y = y, label = label
+        ), size = 7, family = "Girassol",
+        inherit.aes = FALSE,
+        color = "#1d3557",
+        fill = NA,
+        label.size = 0
+    ) +
     theme_minimal() +
-    scale_fill_manual(values = c("red", "green"))
+    scale_fill_manual(values = c("#f03444", "#1d3557")) +
+    scale_color_manual(values = c("#f03444", "#1d3557")) +
+    theme(
+        plot.background = element_rect(fill = "#fdf0d5"),
+        axis.text.x = element_blank(),
+        legend.position = "none",
+        axis.title = element_blank(),
+        axis.text.y = element_text(family = "Girassol")
+    )
+
+setwd("/Users/karatatiwantsinghsidhu/Documents/Code/TidyTuesday/2022/30_October_18")
+
+ggsave(plot = last_plot(), filename = "stranger_things.png", width = 13, height = 10, dpi = 250)
