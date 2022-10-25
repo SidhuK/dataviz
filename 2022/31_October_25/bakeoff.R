3f45d2b37b513fa0b101648a4181160faffb71e1
Karat Sidhu <karatsingh@gmail.com>
2022-10-25 10:39:14 +0530

    bakeoff start

---
+++ 2022/31_October_25/bakeoff.R
@@ -0,0 +1,27 @@
+bakers <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-10-25/bakers.csv")
+
+# variable	class	description
+# series	double	series
+# baker	character	baker
+# star_baker	integer	star_baker
+# technical_winner	integer	technical_winner
+# technical_top3	integer	technical_top3
+# technical_bottom	integer	technical_bottom
+# technical_highest	double	technical_highest
+# technical_lowest	double	technical_lowest
+# technical_median	double	technical_median
+# series_winner	integer	series_winner
+# series_runner_up	integer	series_runner_up
+# total_episodes_appeared	double	total_episodes_appeared
+# first_date_appeared	double	first_date_appeared
+# last_date_appeared	double	last_date_appeared
+# first_date_us	double	first_date_us
+# last_date_us	double	last_date_us
+# percent_episodes_appeared	double	percent_episodes_appeared
+# percent_technical_top3	double	percent_technical_top3
+# baker_full	character	baker_full
+# age	double	age
+# occupation	character	occupation
+# hometown	character	hometown
+# baker_last	character	baker_last
+# baker_first	character	baker_first
