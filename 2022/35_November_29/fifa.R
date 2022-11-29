a87bdb4c310bb20437d89d3bcf8122abd6133a36
Karat Sidhu <karatsingh@gmail.com>
2022-11-29 11:22:19 +0530

    added world cup data

---
+++ 2022/35_November_29/fifa.R
@@ -0,0 +1,2 @@
+wcmatches <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-29/wcmatches.csv")
+worldcups <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-29/worldcups.csv")
