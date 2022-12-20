12935b65ac684bd8a9a2c93d11cc49f963b6760c
Karat Sidhu <karatsingh@gmail.com>
2022-12-20 11:07:46 +0530

    new data

---
+++ 2022/36_December_13/retail.R
@@ -0,0 +1,1 @@
+state_retail <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-12-13/state_retail.csv", col_types = "cciciiccc")
---
+++ 2022/37_December_20/weather.R
@@ -0,0 +1,2 @@
+cities <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-12-20/cities.csv")
+outlook_meanings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-12-20/outlook_meanings.csv")
