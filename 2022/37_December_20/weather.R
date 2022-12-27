22c0e36c009bc1c8f3efb5289cdfc00a75b61d20
Karat Sidhu <karatsingh@gmail.com>
2022-12-27 12:10:59 +0530

    added data

--- 2022/37_December_20/weather.R
+++ 2022/37_December_20/weather.R
@@ -1,2 +1,22 @@
 cities <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-12-20/cities.csv")
 outlook_meanings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-12-20/outlook_meanings.csv")
+
+
+
+library(tidyverse)
+
+
+cities |>
+    gt::gt() |>
+    gtExtras::gt_theme_538()
+
+
+map(cities, class)
+
+
+cities |>
+    group_by(koppen) |>
+    summarise(n()) |>
+    ungroup()
+
+cities |> view()
