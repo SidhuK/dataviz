89f8c6814497e9b4d56459cebcab2d35465a5a9f
Karat Sidhu <karatsingh@gmail.com>
2022-12-29 10:37:56 +0530

    made a waffle chart

--- 2022/38_December_27/starTrek.R
+++ 2022/38_December_27/starTrek.R
@@ -1,8 +1,5 @@
 tlBooks <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-12-27/tlBooks.csv")
 
-# star trek timeline distribution
-# tng, tos, voy, DS9
-
 
 library(tidyverse)
 library(showtext)
@@ -10,50 +7,58 @@ library(ggtext)
 showtext_auto(enable = TRUE)
 showtext_opts(dpi = 250)
 font_add_google("Girassol", "Girassol")
+library(waffle)
 
 
-tlBooks |> View()
-
-tlBooks |>
-    group_by(series) |>
-    summarise(n()) |>
-    view()
 
 
-top4 <- tlBooks |>
-    filter(series == "TNG" | series == "TOS" | series == "DS9" | series == "VOY") |>
-    view()
+# label_position <- top4 |>
+#     group_by(series) |>
+#     summarise(lab_position = mean(year) + 5) |>
+#     mutate(
+#         series_full =
+#             case_when(
+#                 series == "VOY" ~ "Voyager",
+#                 series == "TOS" ~ "The Original Series",
+#                 series == "TNG" ~ "The Next Generation",
+#                 series == "ENT" ~ "Enterprise"
+#             )
+#     )
 
 
-label_position <- top4 |>
-    group_by(series) |>
-    summarise(lab_position = 2400) |>
-    mutate(
-        series_full =
-            case_when(
-                series == "VOY" ~ "Voyager",
-                series == "TOS" ~ "The Original Series",
-                series == "TNG" ~ "The Next Generation",
-                series == "DS9" ~ "Deep Space Nine"
-            )
-    )
+# top4 |> ggplot(aes(x = year, y = series)) +
+#     geom_text(
+#         aes(y = series, x = lab_position, label = series_full),
+#         data = label_position,
+#         family = "Girassol",
+#         color = "#918741",
+#         vjust = 0.05, hjust = 0.01,
+#         size = 10
+#     ) +
+#     ggdist::stat_dots(binwidth = 0.5) +
+#     ggdist::stat_pointinterval(position = "dodge") +
+#     theme_minimal() +
+#     theme(
+#         axis.text.y = element_blank(),
+#         panel.grid.major.y = element_blank(),
+#         panel.grid.minor.y = element_blank()
+#     )
 
 
-top4 |> ggplot(aes(x = year, y = series)) +
-    geom_text(
-        aes(y = series, x = lab_position, label = series_full),
-        data = label_position,
-        family = "Girassol",
-        color = "#918741",
-        vjust = 0.05, hjust = 0.01,
-        size = 10
+
+number_books <- tlBooks |>
+    group_by(series, format) |>
+    summarise(total_books = n())
+
+
+
+number_books |>
+    ggplot(aes(fill = fct_reorder(format, total_books), values = total_books)) +
+    geom_waffle(
+        n_rows = 10,
+        size = 0.33,
+        colour = "white",
+        flip = TRUE
     ) +
-    ggdist::stat_dots(binwidth = 0.45, dotsize = 3) +
-    ggdist::stat_pointinterval(position = "dodge") +
-    scale_x_continuous(limits = c(1990, 2700)) +
-    theme_minimal() +
-    theme(
-        axis.text.y = element_blank(),
-        panel.grid.major.y = element_blank(),
-        panel.grid.minor.y = element_blank()
-    )
+    facet_wrap(~series, nrow = 2, strip.position = "bottom") +
+    theme_minimal()
